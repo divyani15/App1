be0c49eabada91f76c8b5e27c0bf52eb27e5c937
divyani15 <divyan850@gmail.com>
2023-02-02 23:20:47 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [quote, setquote] = useState("");
+  const getQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => setquote(data));
+      
+  };
+  useEffect(() => getQuote(), []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{quote.content}</span>
+      <h4 className="auth">{quote.author}</h4>
+      <button className="btn" onClick={getQuote}>New Quote</button>
+
     </div>
+    
   );
 }
 
