37d78468e0ddc6e2a488ff7feb1222fb0366c40e
divyani15 <87011452+divyani15@users.noreply.github.com>
2023-02-02 23:39:57 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import "./App.css";
 
 function App() {
   const [quote, setquote] = useState("");
+  const [color, setcolor] = useState("");
+  const colors = [
+    "#16a085",
+    "#27ae60",
+    "#2c3e50",
+    "#f39c12",
+    "#e74c3c",
+    "#9b59b6",
+    "#FB6964",
+    "#342224",
+    "#472E32"];
   const getQuote = () => {
     fetch("https://api.quotable.io/random")
       .then((response) => response.json())
       .then((data) => setquote(data));
-      
+      const randomColor = Math.floor(Math.random() * colors.length);
+      setcolor(colors[randomColor]);
   };
   useEffect(() => getQuote(), []);
-
+  useEffect(() => {
+    document.body.style.backgroundColor = color;
+  }, [color]);
   return (
     <div className="App">
       <span>{quote.content}</span>
